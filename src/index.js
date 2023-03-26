d36c0314dfcadbb06d146685d53953d139105276
michelle nderi <nderimichelle@gmail.com>
2023-03-26 18:13:22 +0300

    Done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ document.addEventListener("DOMContentLoaded", () => {
+  let form= document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    createTask(event.target.new-task-description);
+    form.reset();
+  });
 });
+
+function createTask(myTodos) {
+  let li = document.createElement("li");
+  let tasks = document.getElementById("tasks");
+  tasks.appendChild(li);
+  li.textContent = myTodos;
+  let button=document.createElement('button');
+  button.textContent='!'
+  li.appendChild(button);
+  button.addEventListener('click',deleteBtn);
+}
+
+function deleteBtn(event){
+  event.target.parentNode.remove();
+}
