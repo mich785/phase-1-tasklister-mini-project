dbd30b39a195266c165ef368f170c506bf59857c
michelle nderi <nderimichelle@gmail.com>
2023-03-27 13:57:59 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
- document.addEventListener("DOMContentLoaded", () => {
-  let form= document.getElementById("create-task-form");
+// let myTodos= event.target.new-task-description.value
+
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.getElementById("create-task-form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    createTask(event.target.new-task-description);
+    let task = document.getElementById("new-task-description");
+    if (task.value.length > 0) {
+      createTask(task.value);
+    } 
     form.reset();
   });
 });
 
-function createTask(myTodos) {
+function createTask(task) {
   let li = document.createElement("li");
+  li.textContent = task;
+  let button = document.createElement("button");
+  button.textContent = "Remove";
+  li.appendChild(button);
+  button.addEventListener("click", deleteBtn);
+
   let tasks = document.getElementById("tasks");
   tasks.appendChild(li);
-  li.textContent = myTodos;
-  let button=document.createElement('button');
-  button.textContent='!'
-  li.appendChild(button);
-  button.addEventListener('click',deleteBtn);
 }
 
-function deleteBtn(event){
+function deleteBtn(event) {
   event.target.parentNode.remove();
 }
